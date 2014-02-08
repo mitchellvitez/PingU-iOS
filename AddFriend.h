65c723b1ea25cbe072932309b3d3223f5ac7bf4a
Mitchell Vitez <mitchellvitez@gmail.com>
2014-02-08 12:39:27 -0500

    Changed AddFriend class

---
+++ AddFriend.h
@@ -0,0 +1,13 @@
+//
+//  AddFriend.h
+//  PingU
+//
+//  Created by Guest User on 2/8/14.
+//  Copyright (c) 2014 PingU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriend : UIViewController
+
+@end
---
+++ AddFriend.m
@@ -0,0 +1,42 @@
+//
+//  AddFriend.m
+//  PingU
+//
+//  Created by Guest User on 2/8/14.
+//  Copyright (c) 2014 PingU. All rights reserved.
+//
+
+#import "AddFriend.h"
+
+@interface AddFriend(){
+    int isFriend;
+}
+@end
+
+@implementation AddFriend
+
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
